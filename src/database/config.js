ab162d42c0f936ca320f8c9039ba8ba7b91148c2
arthurr-rosante <arrosante@gmail.com>
2026-04-16 10:12:00 -0300

    feat: updated DB config function to look more alike web-data-viz original one

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,32 @@
 import mysql from "mysql2";
 
+export const pool = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+export function execute(query, params = []) {
+    try {
+        pool.connect();
+        const [results] = pool.query(query, params);
+        pool.end();
+        return results;
+    } catch (error) {
+        console.error("ERRO No MySQL Server: ", error.sqlMessage || error.message);
+        throw error;
+    }
+}
+
+
 /*
-Optei por criar um pool no banco de dados ao invés de utilizar o método padrão 
-de conexão em cada uma das queries ao banco, como sugeria a web-data-viz.
-*/ 
+// === VERSÃO SUGERIDA === //
+
+// Optei por criar um pool no banco de dados ao invés de utilizar o método padrão 
+// de conexão em cada uma das queries ao banco, como sugeria a web-data-viz.
+
 export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
@@ -17,12 +40,13 @@ export const pool = mysql.createPool({
     queueLimit: 0               // limite da fila de conexões (0 = fila infinita)
 });
 
-export async function execute(query, params = []) {
+export function execute(query, params = []) {
     try {
-        const [results] = await pool.query(query, params);
+        const [results] = pool.query(query, params);
         return results;
     } catch (error) {
         console.error("ERRO No MySQL Server: ", error.sqlMessage || error.message);
         throw error;
     }
 }
+*/ 
