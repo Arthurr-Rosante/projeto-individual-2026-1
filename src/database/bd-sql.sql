6be0d249028e72fcdaf3b03731048d064c12cfa3
arthurr-rosante <arrosante@gmail.com>
2026-04-17 22:34:38 -0300

    feat: added species table to the database model

--- src/database/bd-sql.sql
+++ src/database/bd-sql.sql
@@ -28,6 +28,7 @@ CREATE TABLE IF NOT EXISTS park(
     PRIMARY KEY (id, idUser),
     CONSTRAINT fkParkUser 		FOREIGN KEY (idUser) REFERENCES `user`(id),
     
+    -- | CHECK Constraints | --
     CONSTRAINT chkParkRating 	CHECK(rating BETWEEN 0 AND 5),
     CONSTRAINT chkParkDinoCoins CHECK(dinoCoins > 0)
 );
@@ -46,16 +47,17 @@ CREATE TABLE IF NOT EXISTS structure(
     PRIMARY KEY (id, idPark),
     CONSTRAINT fkStructurePark FOREIGN KEY (idPark) REFERENCES park(id),
     
+    -- | CHECK Constraints | --
     CONSTRAINT chkStructureType CHECK(type BETWEEN 0 AND 99)
 );
 
 CREATE TABLE IF NOT EXISTS enclosure(
-	id 				INT NOT NULL AUTO_INCREMENT,
-    idStructure 	INT NOT NULL UNIQUE,
+	id 			INT NOT NULL AUTO_INCREMENT,
+    idStructure INT NOT NULL UNIQUE,
     
-    level 			INT NOT NULL DEFAULT 1,
-    durability 		INT NOT NULL DEFAULT 100,
-    hp 				INT NOT NULL,
+    level 		INT NOT NULL DEFAULT 1,
+    durability 	INT NOT NULL DEFAULT 100,
+    hp 			INT NOT NULL,
     
     createdAt   DATETIME DEFAULT CURRENT_TIMESTAMP(),
     updatedAt   DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
@@ -63,26 +65,47 @@ CREATE TABLE IF NOT EXISTS enclosure(
     PRIMARY KEY (id, idStructure),
     CONSTRAINT fkEnclosureStructure FOREIGN KEY (idStructure) REFERENCES structure(id),
     
+    -- | CHECK Constraints | --
     CONSTRAINT chkEnclosureLevel CHECK(level IN(1, 2, 3))
 );
 
 CREATE TABLE IF NOT EXISTS dinosaur(
-	id INT NOT NULL,
+	id          INT NOT NULL AUTO_INCREMENT,
     idEnclosure INT NOT NULL UNIQUE,
-    
-    species VARCHAR(120) NOT NULL,
+    idSpecies   INT NOT NULL, 
     
     createdAt   DATETIME DEFAULT CURRENT_TIMESTAMP(),
     updatedAt   DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
 
     PRIMARY KEY (id, idEnclosure),
     CONSTRAINT fkDinosaurEnclosure FOREIGN KEY (idEnclosure) REFERENCES enclosure(id),
-    
-    CONSTRAINT chkDinosaurSpecies CHECK(species IN(
-		"ceratossauro",		"tiranossauro",		"espinossauro",
-        "giganotossauro",	"velociraptor",		"dilofossauro",
-        "herrerassauro",	"compsognathus",	"coritossauro",
-        "triceratopo",		"estegossauro",		"parassaurolofo",
-        "anquilossauro",	"braquiossauro", 	"brontossauro"
-	))
+    CONSTRAINT fkDinosaurSpecies   FOREIGN KEY (idSpecies)   REFERENCES species(id)
+);
+
+CREATE TABLE IF NOT EXISTS species(
+    id                  INT NOT NULL AUTO_INCREMENT,
+
+    name                VARCHAR(120) NOT NULL,
+    description         VARCHAR(255) NOT NULL,
+    period              VARCHAR(60) NOT NULL,
+    height              DECIMAL(10, 3) NOT NULL,
+    weight              DECIMAL (10, 3) NOT NULL,
+    diet                VARCHAR(60) NOT NULL,
+    aggressiveness      INTEGER NOT NULL,
+    hatchCost           INTEGER NOT NULL,
+    hatchTimeInSeconds  INTEGER NOT NULL,
+    hatchSuccessRatio   INTEGER NOT NULL,
+
+    createdAt   DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    updatedAt   DATETIME DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+
+    PRIMARY KEY (id),
+
+    -- | CHECK Constraints | --
+    CONSTRAINT chkSpeciesPeriod CHECK(period IN('cretaceous', 'jurassic', 'triassic')),
+    CONSTRAINT chkSpeciesHeightAndWeight CHECK(height > 0 AND weight > 0),
+    CONSTRAINT chkSpeciesAggressiveness CHECK(aggressiveness BETWEEN 0 AND 1),
+    CONSTRAINT chkSpeciesHatchCostAndTime CHECK(hatchCost > 0 AND hatchTimeInSeconds > 0),
+    CONSTRAINT chkSpeciesHatchSuccessRatio CHECK(hatchSuccessRatio BETWEEN 0 AND 1)
+
 );
