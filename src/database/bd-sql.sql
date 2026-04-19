c0b5d253fc94e2b68e2385e6db65054cd99fc3e3
arthurr-rosante <arrosante@gmail.com>
2026-04-19 13:10:48 -0300

    feat: added inserts into bd-sql.sql

--- src/database/bd-sql.sql
+++ src/database/bd-sql.sql
@@ -90,7 +90,7 @@ CREATE TABLE IF NOT EXISTS species(
     PRIMARY KEY (id),
 
     -- | CHECK Constraints | --
-	CONSTRAINT chkSpeciesDiet CHECK(diet IN('herbivorous', 'carnivorous', 'omnivorous')),
+	CONSTRAINT chkSpeciesDiet CHECK(diet IN('herbívoro', 'carnívoro', 'onívoro')),
     CONSTRAINT chkSpeciesHeightAndWeight CHECK(height > 0 AND weight > 0),
     CONSTRAINT chkSpeciesAggressiveness CHECK(aggressiveness BETWEEN 0 AND 1),
     CONSTRAINT chkSpeciesHatchCostAndTime CHECK(hatchCost > 0 AND hatchTimeInSeconds > 0),
@@ -110,3 +110,32 @@ CREATE TABLE IF NOT EXISTS dinosaur(
     CONSTRAINT fkDinosaurEnclosure FOREIGN KEY (idEnclosure) REFERENCES enclosure(id),
     CONSTRAINT fkDinosaurSpecies   FOREIGN KEY (idSpecies)   REFERENCES species(id)
 );
+
+-- | POPULANDO TABELA 'species' | --
+
+INSERT INTO species (
+    name, 
+    description, 
+    temporalRange, 
+    locomotionType, 
+    height, 
+    weight, 
+    diet, 
+    aggressiveness, 
+    hatchCost, 
+    hatchTimeInSeconds, 
+    hatchSuccessRatio
+) VALUES -- name		description                                                                                                             temporalRange           locomotionType          height(m)   weight(kg)  diet        aggr   hchCost hchTime hchSuccess 
+    ('Compsognathus', 	'Um gênero de pequenos dinossauros terópodes carnívoros bípedes do Jurássico Superior.', 								'Jurássico Superior', 	'Bípede', 				0.260, 		3.000, 		'carnívoro', 	1, 	500, 	30, 	1),
+    ('Tiranossauro', 	'Um gênero de grandes dinossauros terópodes do Cretáceo Superior, amplamente conhecido como T. rex.', 					'Cretáceo Superior', 	'Bípede', 				4.000, 		8000.000, 	'carnívoro', 	1, 	10000, 	600, 	1),
+    ('Espinossauro', 	'Um gênero de dinossauro espinossaurídeo que viveu no que hoje é o Norte da África durante o Cretáceo Superior.', 		'Cretáceo Superior', 	'Bípede', 				4.500, 		14000.000, 	'carnívoro', 	1, 	12000, 	600, 	1),
+    ('Braquiossauro', 	'Um gênero de dinossauro saurópode que viveu na América do Norte durante o Jurássico Superior.', 						'Jurássico Superior', 	'Quadrúpede', 			12.000, 	40000.000, 	'herbívoro', 	0, 	8000, 	720, 	1),
+    ('Coritossauro', 	'Um gênero de dinossauro hadrossaurídeo do período Cretáceo Superior, conhecido por sua crista distinta.', 				'Cretáceo Superior', 	'Bípede/Quadrúpede', 	2.500, 		3000.000, 	'herbívoro', 	0, 	3000, 	180, 	1),
+    ('Parassaurolofo', 	'Um dinossauro ornitópode herbívoro que viveu na América do Norte durante o Cretáceo Superior.', 						'Cretáceo Superior', 	'Bípede/Quadrúpede', 	3.000, 		2500.000, 	'herbívoro', 	0, 	3500, 	180, 	1),
+    ('Triceratops', 	'Um gênero de dinossauro ceratopsídeo herbívoro que surgiu durante o Cretáceo Superior.', 								'Cretáceo Superior', 	'Quadrúpede', 			3.000, 		9000.000, 	'herbívoro', 	0, 	5000, 	300, 	1),
+    ('Velociraptor', 	'Um gênero de dinossauro terópode dromeossaurídeo que viveu durante a última parte do Cretáceo.', 						'Cretáceo Superior', 	'Bípede', 				0.500, 		15.000, 	'carnívoro', 	1, 	2000, 	90, 	1),
+    ('Dilofossauro', 	'Um gênero de dinossauros terópodes que viveu na América do Norte durante o Jurássico Inferior.', 						'Jurássico Inferior', 	'Bípede', 				2.000, 		400.000, 	'carnívoro', 	1, 	2500, 	90, 	1),
+    ('Anquilossauro', 	'Um gênero de dinossauro blindado do período Cretáceo Superior, conhecido por sua cauda pesada em forma de clava.', 	'Cretáceo Superior', 	'Quadrúpede', 			1.700, 		6000.000, 	'herbívoro', 	0, 	4500, 	300, 	1),
+    ('Ceratossauro', 	'Um dinossauro terópode predador do período Jurássico Superior, possuindo um chifre proeminente.', 						'Jurássico Superior', 	'Bípede', 				2.000, 		980.000, 	'carnívoro', 	1, 	3000, 	300, 	1),
+    ('Estegossauro', 	'Um gênero de dinossauro herbívoro quadrúpede e blindado do período Jurássico Superior.', 								'Jurássico Superior', 	'Quadrúpede', 			2.700, 		5300.000, 	'herbívoro', 	0, 	4000, 	300, 	1);
+    
