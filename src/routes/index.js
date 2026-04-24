ed3eda37933a77a2ccc3b3ec4b84a674768d6a2d
arthurr-rosante <arrosante@gmail.com>
2026-04-23 21:38:23 -0300

    feat: added all entities routes to the main app router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,23 @@
 import express from "express";
 
-import {userRoutes} from "./users.js";
+import { userRoutes } from "./users.js";
+import { parkRoutes } from "./parks.js";
+import { tileRoutes } from "./tiles.js";
+import { speciesRoutes } from "./species.js";
+import { eventRoutes } from "./events.js";
+import { buildingRoutes } from "./buildings.js";
+import { enclosureRoutes } from "./enclosures.js";
+import { dinosaurRoutes } from "./dinosaurs.js";
 
 // === Router CENTRALIZADOR === //
 export const routes = express.Router();
 
 // === ROTAS DA APLICAÇÃO === //
 routes.use("/users", userRoutes);
-// routes.use("/parks", parkRoutes);
-// routes.use("/structures", structureRoutes);
-// routes.use("/enclosures", enclosureRoutes);
-// routes.use("/dinosaurs", dinosaurRoutes);
+routes.use("/parks", parkRoutes);
+routes.use("/tiles", tileRoutes);
+routes.use("/species", speciesRoutes);
+routes.use("/events", eventRoutes);
+routes.use("/buildings", buildingRoutes);
+routes.use("/enclosures", enclosureRoutes);
+routes.use("/dinosaurs", dinosaurRoutes);
