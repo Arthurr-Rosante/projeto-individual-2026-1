354dda7c4570b7718baa9552d2bf6a87662decd0
arthurr-rosante <arrosante@gmail.com>
2026-04-26 23:37:04 -0300

    feat: added a new 'getByEmail' method in userModel and filtered which fields are allowed to be returned by the select statement

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,11 @@
 import { execute } from "../database/config.js";
 
+const allowedFields = "id, username, email, createdAt, updatedAt";
+
 // === INSTRUÇÃO: REGISTRAR === //
 export function register(username, email, password) {
     const instruction = 
-        "INSERT INTO user(username, email, password) VALUES(?, ?, ?)";
+    "INSERT INTO user(username, email, password) VALUES(?, ?, ?)";
     
     console.log("\n[userModel.js | register] - Executando INSERT...");
     console.log(`\n[userModel.js | register] - Instrução: "${instruction}"`);
@@ -14,10 +16,21 @@ export function register(username, email, password) {
 // === INSTRUÇÃO: AUTENTICAR === //
 export function authenticate(email, password) {
     const instruction = 
-        "SELECT * FROM user WHERE email = ? AND password = ?";
+        `SELECT ${allowedFields} FROM user WHERE email = ? AND password = ?`;
     
     console.log("\n[userModel.js | authenticate] - Executando SELECT...");
     console.log(`\n[userModel.js | authenticate] - Instrução: "${instruction}"`);
     
     return execute(instruction, [email, password]);
+}
+
+// === INSTRUÇÃO: PROCURAR POR EMAIL === //
+export function getByEmail(email) {
+    const instruction = 
+    `SELECT ${allowedFields} FROM user WHERE email = ?`;
+    
+    console.log("\n[userModel.js | getByEmail] - Executando SELECT...");
+    console.log(`\n[userModel.js | getByEmail] - Instrução: "${instruction}"`);
+    
+    return execute(instruction, [email]);
 }
