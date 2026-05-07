8023e7c14f95ecae4af4de84619e908cfe4fa39c
arthurr-rosante <arrosante@gmail.com>
2026-05-07 18:34:59 -0300

    feat: created timer factory that returns functions for dealing with setInterval

---
+++ public/scripts/utils/timer.js
@@ -0,0 +1,50 @@
+const DECREASE_INTERVAL = 1000;
+const SHOW_TIMER_LOGS = false;
+function createTimer(fn, interval) {
+    if(typeof fn !== "function") {
+        console.error("[timer.js] Erro: Paramêtro 'fn' não é uma função!");
+        return;
+    }
+
+    let currentInterval = interval;
+    let timerId = null;
+
+    const start = () => {
+        if(timerId !== null) return;
+
+        timerId = setInterval(() => {
+            currentInterval -= DECREASE_INTERVAL;
+            if(currentInterval <= 0) {
+                fn();
+                currentInterval = interval;
+            }
+            
+            SHOW_TIMER_LOGS && console.log(`[timerId = ${timerId}] RODOU | intervalo: ${currentInterval}`)
+        }, DECREASE_INTERVAL);
+    };
+    
+    const pause = () => {
+        SHOW_TIMER_LOGS && console.log(`[timerId = ${timerId}] PAUSOU | intervalo: ${currentInterval}`)
+
+        clearInterval(timerId);
+        timerId = null;
+    };
+    
+    const resume = () => {
+        SHOW_TIMER_LOGS && console.log(`[timerId = ${timerId}] RETOMOU | intervalo: ${currentInterval}`)
+        start();
+    };
+    
+    const reset = () => {
+        SHOW_TIMER_LOGS && console.log(`[timerId = ${timerId}] RESETOU | intervalo: ${currentInterval}`)
+
+        pause();
+        currentInterval = interval;
+        start();
+    };
+
+    const getCurrentInterval = () => currentInterval;
+
+    start();
+    return {timerId, start, pause, resume, reset, getCurrentInterval};
+}
