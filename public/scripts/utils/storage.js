10cfc974f296ea795d0c1354c817cafa0c200846
arthurr-rosante <arrosante@gmail.com>
2026-05-10 18:27:32 -0300

    refactor: storage functions are much easier to use and deal with stringify and parse on their own

--- public/scripts/utils/storage.js
+++ public/scripts/utils/storage.js
@@ -1,16 +1,42 @@
-function setItem(key, value) {
-    sessionStorage.setItem(key, value);
+export const storage = {
+    set: (key, value) => {
+        try {
+            const stringifiedValue = JSON.stringify(value);
+            sessionStorage.setItem(key, stringifiedValue);
+        } catch (error) {
+            console.error(`[storage.js] Erro ao salvar no sessionStorage (Chave: ${key}): ${error}`);
+        }
+    },
+    get:(key) => {
+        try {
+            const result = sessionStorage.getItem(key);
+            return result ? JSON.parse(result) : null;
+        } catch (error) {
+            console.error(`[storage.js] Erro ao ler sessionStorage (Chave: ${key}): ${error}`);
+            return null;
+        }
+    },
+    remove:(key) => {
+        sessionStorage.removeItem(key);
+    },
+    clear:() => {
+        sessionStorage.clear();
+    },
 };
 
-function getItem(key) {
-    return sessionStorage.getItem(key);
-};
+// function setItem(key, value) {
+//     sessionStorage.setItem(key, value);
+// };
 
-function clearItem(key) {
-    if(!getItem(key)) return;
-    sessionStorage.removeItem(key);
-}
+// function getItem(key) {
+//     return sessionStorage.getItem(key);
+// };
 
-function clearStorage() {
-    sessionStorage.clear();
-}
+// function clearItem(key) {
+//     if(!getItem(key)) return;
+//     sessionStorage.removeItem(key);
+// }
+
+// function clearStorage() {
+//     sessionStorage.clear();
+// }
