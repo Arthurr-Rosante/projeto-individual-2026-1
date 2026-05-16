307ad4b5eb078e4ad380824538377d7c93924d27
arthurr-rosante <arrosante@gmail.com>
2026-05-16 12:31:52 -0300

    feat: added an actual save logic to the save function

--- public/scripts/event-trigger.js
+++ public/scripts/event-trigger.js
@@ -2,30 +2,59 @@ import { toast } from "./utils/toast.js";
 import { storage } from "./utils/storage.js";
 import { updateParkBalance } from "./update-park.js";
 import { getResourceFromStorage } from "./utils/getResourceFromStorage.js";
+import { loadDevScreen, loadStatusBar, loadGrid, loadHatchery, loadGeneral } from "./loaders/index.js";
+
+let isSaving = false;
+let isPoppingEvent = false;
 
 export const events = {
-    save: () => {
-        console.log("SALVOU! ", new Date().toLocaleString());
-        const gameData = storage.get("JPWG_DATA");
-        
-        if(!gameData) {
-            console.warn("[save] Aviso: Objeto JPWG_DATA está incompleto...");
-            return;
-        }
+    save: async () => {
+        try {
+            isSaving = true;
+            const gameData = storage.get("JPWG_DATA");
+            if(!gameData) {
+                throw new Error("Não foi possível retornar dados do sessionStorage. Tente logar-se novamente...");
+            }
+            
+            const {user, park} = gameData;
+            // 1. Atualiza Parque
+            await fetch(`/api/parks/${park.idUser}/update`, {
+                method: "PUT", 
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(park)
+            })
 
-        // Salvando dados no banco
-        
-        toast({
-            variant: "success",
-            title: "Jogo Salvo!"
-        });
-        events.copyStorage("JPWG_DATA");
-    },
-    copyStorage: (key) => {
-        const data = storage.get(key);
-        storage.set(`${key}_COPY`, data);
+            // 2. Retorna dados mais recentes do BD
+            const response = await fetch(`/api/users/${user.id}/data`);
+            if(!response.ok) {
+                throw new Error("Não foi possível retornar dados do Banco de Dados. Tente logar-se novamente...");
+            }
 
-        console.log("COPIOU! ", new Date().toLocaleString());
+            const freshGameData = await response.json();
+
+            // 3. Atualiza sessionStorage
+            storage.set("JPWG_DATA", {user, ...freshGameData});
+            toast({
+                variant: "success",
+                title: "Jogo Salvo!",
+            })
+
+            // 4. Recarrega a UI
+            loadDevScreen();
+            loadStatusBar();
+            loadGrid();
+            loadHatchery();
+            loadGeneral();
+       } catch (error) {
+            console.error(error);
+            toast({
+                variant: "destructive",
+                title: "Erro ao Salvar",
+                message: error.message || error
+            });
+       } finally {
+            isSaving = false;
+       }
     },
     increaseBalance: () => {
         const { park } = storage.get("JPWG_DATA");
@@ -56,3 +85,69 @@ export const events = {
     sabotage: () => {},
     pouringRain: () => {}
 };
+
+/*
+    save: () => {
+        if(isSaving) {
+            toast({
+                variant: "warn",
+                title: "Aguarde Salvar!",
+                message: "O Jogo está sendo salvo neste momento."
+            });
+            return;
+        };
+
+        const gameData = storage.get("JPWG_DATA");
+        if(!gameData) {
+            //aviso...
+            return;
+        }
+
+        // Copia storage atual e salva no banco
+        events.copyStorage("JPWG_DATA");
+        const copiedData = storage.get("JPWG_DATA_COPY");
+        if(!copiedData) {
+            // aviso...
+            return;
+        }
+        
+        fetch("/api/users/save", {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(copiedData)
+        })
+        .then((res) => {
+            isSaving = true;
+
+            if(!res.ok) {
+                // erro...
+                throw new Error("erro ao salvar...");
+            }
+            return res.json();
+        })
+        .then((data) => {
+            storage.set("JPWG_DATA", data);
+            toast({
+                variant: "success",
+                title: "Jogo Salvo!"
+            });
+
+            // atualiza dados da UI
+            loadDevScreen();
+            loadStatusBar();
+            loadGrid();
+            loadHatchery();
+            loadGeneral();
+        })
+        .catch((error) => {
+            toast({
+                variant: "destructive",
+                title: "Erro ao Salvar!",
+                message: error.message || error
+            });
+        })
+        .finally(() => {
+            isSaving = false;
+        });
+    },
+*/ 
